bda5d699b4f05266cdfb76f9e9af7653bd57f8ae
shahbaz20xx <chshahaz108@gmail.com>
2025-05-21 16:29:15 +0500

    Changed Database Seeder to Seed Dummy Data to Tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Application;
+use App\Models\Invoice;
+use App\Models\Project;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +21,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(VoyagerDatabaseSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->admin()->create([
+            'name' => 'Shahbaz',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'),
+        ]);
+
+        User::factory()->regularUser()->count(20)->create();
+
+        Project::factory()->count(30)->create();
+
+        Task::factory()->count(50)->create();
+        
+        Application::factory()->count(150)->create();
+
+        Invoice::factory()->count(100)->create();
+
+        $projects = Project::all();
+        foreach ($projects as $project) {
+            $acceptedApplication = $project->applications()->where('status', 'accepted')->first();
+
+            if ($acceptedApplication) {
+                $project->applications()
+                    ->where('id', '!=', $acceptedApplication->id)
+                    ->where('status', 'pending')
+                    ->update(['status' => 'rejected']);
+            }
+        }
+
+        $this->command->info('Database seeded successfully!');
     }
 }
