d63582d9be2f58087f5f8992fb01cedd047b1829
shahbaz20xx <chshahaz108@gmail.com>
2025-05-23 17:13:05 +0500

    Added Login and Registration Pages

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -2,9 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserAccountController extends Controller
 {
-    //
+    public function registration()
+    {
+        return view('account.registration');
+    }
+
+    public function processRegistration(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:5|same:confirm_password',
+            'confirm_password' => 'required',
+        ]);
+
+        if ($validator->passes()) {
+
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->save();
+
+            $message = "You have registered successfully.";
+            Session()->flash('success', $message);
+
+            return response()->json([
+                'status' => true,
+                'message' => $message,
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+    }
+
+    public function login()
+    {
+        return view('account.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:5',
+        ]);
+        if ($validator->passes()) {
+
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                return redirect()->route('home');
+            } else {
+                return redirect()->route('account.login')->with('error', 'Either email/password is incorrect');
+            }
+        } else {
+            return redirect()->route('account.login')
+                ->withErrors($validator)
+                ->withInput($request->only('email'));
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
+use TCG\Voyager\Facades\Voyager;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +17,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/',[HomeController::class,'index'])->name('home');
+Route::get('/projects',[ProjectsController::class,'index'])->name('projects');
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+});
+
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/register',[UserAccountController::class,'registration'])->name('account.registration');
+    Route::post('/process-register',[UserAccountController::class,'processRegistration'])->name('account.processRegistration');
+    Route::get('/login',[UserAccountController::class,'login'])->name('account.login');
+    Route::post('/authenticate',[UserAccountController::class,'authenticate'])->name('account.authenticate');
 });
